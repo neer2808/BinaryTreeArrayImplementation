34295ffba8b240693dbe7cfdaa4c146454d7c67d
neer2808 <uic.neerajkhanna@gmail.com>
2020-02-29 13:12:46 +0530

    Add method to insert a new node
    and write the logic for level order traversal

--- src/BinaryTreeWithArray.java
+++ src/BinaryTreeWithArray.java
@@ -1,9 +1,32 @@
 public class BinaryTreeWithArray {
 
   int arr[];
-
-  public static void main(String[] args) {
-
+  int lastindex;
+ // Constructor to create blank tree
+  public BinaryTreeWithArray(int size)
+  {
+    arr= new int[size];
+    lastindex = 0;
   }
-
+  // method to insert the value
+  public void insert(int value)
+  {
+    if(arr.length-1 == lastindex)
+    {
+      System.out.println("array is full");
+    }
+    else
+    {
+      arr[lastindex+1]= value;
+      lastindex++;
+    }
+  }
+  // method for inorder traversal
+  public  void levelordertraversal()
+  {
+    for(int i=1;i<=lastindex;i++)
+    {
+      System.out.println(arr[i]);
+    }
+  }
 }
---
+++ src/BinaryTreewithArraymain.java
@@ -0,0 +1,9 @@
+public class BinaryTreewithArraymain {
+  public static void main(String[] args) {
+    BinaryTreeWithArray obj = new BinaryTreeWithArray(10);
+    obj.insert(10);
+    obj.insert(20);
+    obj.insert(5);
+    obj.levelordertraversal();
+  }
+}
